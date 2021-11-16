e144b1cf093d42fd8a534987ce3b197ee79a1c8f
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-17 01:22:44 +0700

    component route and card

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,18 +3,48 @@
               <CFlex justify-content="start" align-items="start">
                      <CText font-size="20px" font-weight="800" mt="18px">
                             <CFlex w="10rem">
-                                   <span style="color: #50b9ff;">HJ Corporate&trade;</span>
+                                   <NuxtLink to="/">
+                                          <span style="color: #50b9ff;">HJ Corporate&trade;</span>
+                                   </NuxtLink>
                             </CFlex>
                      </CText>
                      <NuxtLink to="/">
-                            <CText font-size="xs" mx="10px" font-weight="bold" mt="24px" textTransform="uppercase">Homepage</CText>
+                            <CText font-size="xs" mx="16px" font-weight="bold" mt="24px" textTransform="uppercase">Homepage</CText>
                      </NuxtLink>
                      <NuxtLink to="/components">
-                            <CText font-size="xs" mx="10px" font-weight="bold" textTransform="uppercase" mt="24px">Components</CText>
+                            <CText font-size="xs" mx="16px" font-weight="bold" textTransform="uppercase" mt="24px">Components</CText>
                      </NuxtLink>
                      <NuxtLink to="/about">
-                            <CText font-size="xs" mx="10px" font-weight="bold" textTransform="uppercase" mt="24px">About</CText>
+                            <CText font-size="xs" mx="16px" font-weight="bold" textTransform="uppercase" mt="24px">About</CText>
                      </NuxtLink>
+                     <CMenu>
+                     <CMenuButton>
+                            Your Cats
+                     </CMenuButton>
+                     <CMenuList>
+                     <CMenuItem h="48px">
+                            <CImage
+                                   size="2rem"
+                                   rounded="full"
+                                   src="https://placekitten.com/100/100"
+                                   alt="Fluffybuns the destroyer"
+                                   mr="12px"
+                            />
+                     <span>Fluffybuns the Destroyer</span>
+                     </CMenuItem>
+                     <CMenuItem h="40px">
+                     <CImage
+                     size="2rem"
+                     rounded="full"
+                     src="https://placekitten.com/120/120"
+                     alt="Simon the pensive"
+                     mr="12px"
+                     />
+                     <span>Simon the pensive</span>
+                     </CMenuItem>
+                     </CMenuList>
+                     </CMenu>
+
               </CFlex>
        </CBox>
 </template>
---
+++ pages/components/Card.vue
@@ -0,0 +1,47 @@
+<template>
+       <div class="container">
+		<CBox w="auto" h="100vh" justify-content="center" mx="100px" pt="140px">
+                     <CBox max-w="32rem" mx="auto" align="center">
+                            <CHeading font-size="5xl" mb="4">
+                                          Card Components
+                            </CHeading>
+                            <CText font-size="md">
+                                   Card component adalah component yang digunakan untuk menampilkan suatu produk.
+                            </CText>
+                     </CBox>
+		</CBox>
+	</div>
+</template>
+
+<script>
+import { CText, CBox } from  "@chakra-ui/vue"
+export default {
+       head() {
+		return {
+			title: this.title,
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+					{
+						hid: 'hjcorporate - card components',
+						name: 'cardcomponents',
+						content: 'Card Components'
+					}
+			],
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+		}
+	},
+       data () {
+		return {
+			title: "HJ Corporate - Card Components"
+		}
+	},
+       name: "Product",
+       components: {
+              CText,
+              CBox
+       }
+}
+</script>
+
+<style></style>
