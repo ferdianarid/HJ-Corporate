3ec817832ac4f45827fee4e6d730e0c527d58992
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-16 11:02:56 +0700

    initial commit setup

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="container">
+		<CBox
+			d="flex"
+			w="100vw"
+			h="100vh"
+			flex-dir="column"
+			justify-content="center"
+		>
+			<CHeading text-align="center" mb="4">
+				⚡️ Hello chakra-ui/vue
+			</CHeading>
+		</CBox>
+	</div>
+</template>
+
+<script lang="js">
+import {
+	CBox,
+	CFlex,
+	CHeading
+} from '@chakra-ui/vue'
+
+export default {
+	name: 'App',
+	components: {
+		CBox,
+		CFlex,
+		CHeading
+	},
+	data () {
+		return {}
+	},
+	computed: {},
+	methods: {}
+}
+</script>
